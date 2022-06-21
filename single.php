eb2de57aa0227488cb378a4289e37894e0cecc92
James Pederson <james@jpederson.com>
2022-06-20 22:01:21 -0500

    Adjust some code formatting.

--- single.php
+++ single.php
@@ -10,27 +10,16 @@ get_header();
 the_showcase();
 
 ?>
-	<!--
-	<?php if ( in_category( 'advocacy-on-the-move' ) ) { ?>
-	<div class="page-title aotm">
-		<h1>Advocacy on the Move</h1>
-	</div>
-	<?php } else { ?>
-	<div class="page-title anthem">
-		<h1>Anthem</h1>
-	</div>
-	<?php } ?>
-	-->
 	<div class="two-column" role="main">
 		<div class="sidebar">
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('newsletter') ) : ?><!-- no sidebar --><?php endif; ?>
 		</div>
 		<div class="right-column">
 			<div class="right-column-inner">
-		<?php 
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post(); 
-				?>
+			<?php 
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post(); 
+					?>
 				<h1><?php the_title(); ?></h1>
 				<p class="post-date"><?php the_date(); ?></p>
 				<div class="featured-image">
@@ -39,14 +28,13 @@ the_showcase();
 				</div>
 				<?php the_content(); ?>
 				<p class="quiet">Posted in <?php print get_the_category_list( ', ' ) ?>.</p>
-				<?php
-			endwhile;
-		endif;
-		?>
+					<?php
+				endwhile;
+			endif;
+			?>
 			</div>
 		</div>
 	</div>
-
 <?php
 
 get_footer();
