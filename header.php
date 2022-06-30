dedea2f6123022b21cb736455a5a614f77fc4970
James Pederson <james@jpederson.com>
2022-06-29 22:16:30 -0500

    Add people styles.

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <![endif]-->
 
 <?php wp_head(); ?>
-<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=3" rel="stylesheet" type="text/css">
+<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=4" rel="stylesheet" type="text/css">
 
 </head>
 <body <?php body_class(); ?>>
