3c5d9e9a720e91595b0e08c978fd36db21b93187
James Pederson <james@jpederson.com>
2022-07-01 15:18:19 -0500

    Open the icons on the homepage in new tabs on mobile, and in lightbox on larger screens.

--- js/src/main.js
+++ js/src/main.js
@@ -39,16 +39,25 @@ jQuery(document).ready(function($){
 	// handle icon clicks
 	$( '.icons .icon' ).on( 'click', function(){
 		var icon = $(this);
-		if ( icon.hasClass( 'lightbox-icon' ) ) {
-			$.magnificPopup.open({
-				items: {
-					src: icon.attr( 'data-href' )
-				},
-				type: 'iframe'
-			});
+
+		// if the window is smaller than tablet portait
+		if ( $(window).width() < 768 ) {
+
+			window.open( icon.attr( 'data-href' ), '_blank' );
+
 		} else {
-			location.href = icon.attr( 'data-href' );
+			if ( icon.hasClass( 'lightbox-icon' ) ) {
+				$.magnificPopup.open({
+					items: {
+						src: icon.attr( 'data-href' )
+					},
+					type: 'iframe'
+				});
+			} else {
+				location.href = icon.attr( 'data-href' );
+			}
 		}
+
 	});
 
 
