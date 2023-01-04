993fe64c6c61470845856188616f747ef122ebb5
James Pederson <james@jpederson.com>
2023-01-04 03:39:51 -0600

    Update js.

--- js/src/events.js
+++ js/src/events.js
@@ -50,39 +50,44 @@ jQuery(document).ready(function($){
 	}
 
 
-	// fixed registration
-	$( window ).on( 'scroll', function(){
+	// if we're on an individual event page
+	if ( $( 'body' ).hasClass( 'event' ) ) {
 
-		// function to figure if we've scrolled to the content
-		var past_content = function() {
-			return $( window ).width() >= 768 && $(window).scrollTop() >= $( '.after-showcase' ).offset().top ? true : false;
-		}
+		// fixed registration
+		$( window ).on( 'scroll', function(){
 
-		// function to figure if we reached the footer
-		var footer_reached = function() {
-			return $( window ).width() >= 768 && $(window).scrollTop() >= ( $( '.footer' ).offset().top - $( window ).height() ) ? true : false;
-		}
+			// function to figure if we've scrolled to the content
+			var past_content = function() {
+				return $( window ).width() >= 768 && $(window).scrollTop() >= $( '.after-showcase' ).offset().top ? true : false;
+			}
 
-		// if we're past the showcase and before the footer
-		if ( past_content() && !footer_reached() ) {
+			// function to figure if we reached the footer
+			var footer_reached = function() {
+				return $( window ).width() >= 768 && $(window).scrollTop() >= ( $( '.footer' ).offset().top - $( window ).height() ) ? true : false;
+			}
 
-			// get the event reg height
-			var event_reg_height = $( '.event-registration' ).height() + 40;
+			// if we're past the showcase and before the footer
+			if ( past_content() && !footer_reached() ) {
 
-			// fixed event reg
-			$( '.event-registration' ).addClass( 'fixed' );
+				// get the event reg height
+				var event_reg_height = $( '.event-registration' ).height() + 40;
 
-			// margin on footer to stop flicker
-			$( 'footer' ).css( 'margin-top', event_reg_height+'px' );
+				// fixed event reg
+				$( '.event-registration' ).addClass( 'fixed' );
 
-		} else {
+				// margin on footer to stop flicker
+				$( 'footer' ).css( 'margin-top', event_reg_height+'px' );
 
-			// default mode
-			$( '.event-registration' ).removeClass( 'fixed' );
-			$( 'footer' ).css( 'margin-top', 0 );
-			
-		}
-	});
+			} else {
+
+				// default mode
+				$( '.event-registration' ).removeClass( 'fixed' );
+				$( 'footer' ).css( 'margin-top', 0 );
+				
+			}
+		});
+
+	}
 
 });
 
