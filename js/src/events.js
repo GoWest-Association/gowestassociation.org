0ea137756294965814ba004b2d94462a202175f1
James Pederson <james@jpederson.com>
2023-01-05 21:46:04 -0600

    Remove event list height override.

--- js/src/events.js
+++ js/src/events.js
@@ -20,35 +20,6 @@ jQuery(document).ready(function($){
 		location.href = $.query.set( "mo", $(this).data('month') ).set( "yr", $(this).data('year') );
 	});
 
-	// home events list minimum height
-	var set_event_height = function(){
-
-		// reset min-height
-		$( 'body.home .event-list' ).css( 'min-height', 'auto' );
-
-		// if the device is tablet or larger
-		if ( $(window).width() >= 768 ) {
-
-			// set the min-height of the event list			
-			$( 'body.home .event-list' ).css( 'min-height', $( 'body.home .article-cards .entry:first-child').outerHeight() + 27 );
-
-		}
-	}
-
-	// if we're om the homepage
-	if ( $( 'body.home' ).length > 0 ) {
-
-		// set event list min-height on load
-		setTimeout(function(){
-			set_event_height();
-		}, 1000 );
-
-
-		// set event list min-height on window resize
-		$(window).resize( set_event_height );
-
-	}
-
 
 	// if we're on an individual event page
 	if ( $( 'body' ).hasClass( 'event' ) ) {
