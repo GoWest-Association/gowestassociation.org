7abb7948a7f9d9f0288599e67b7c915ac39a6fa2
James Pederson <james@jpederson.com>
2023-01-10 19:17:18 -0600

    Fix for menu shortcode to allow 'id' param.

--- library/shortcode/menu.php
+++ library/shortcode/menu.php
@@ -3,42 +3,16 @@
 // Function that will return our Wordpress menu
 function menu_shortcode( $atts, $content = null ) {
 	
-    extract( shortcode_atts( array(
-		'menu'            => '', 
-		'container'       => 'div', 
-		'container_class' => '', 
-		'container_id'    => '', 
-		'menu_class'      => 'menu', 
-		'menu_id'         => '',
-		'echo'            => true,
-		'fallback_cb'     => 'wp_page_menu',
-		'before'          => '',
-		'after'           => '',
-		'link_before'     => '',
-		'link_after'      => '',
-		'depth'           => 0,
-		'walker'          => '',
-		'theme_location'  => ''),
-		$atts
-    ) );
- 
+	print_r( $atts );
+
 	// return the menu content 
 	return wp_nav_menu( array( 
-		'menu'            => $menu, 
-		'container'       => $container, 
-		'container_class' => $container_class, 
-		'container_id'    => $container_id, 
-		'menu_class'      => $menu_class, 
-		'menu_id'         => $menu_id,
+		'menu'            => $atts['id'], 
+		'container'       => 'div', 
+		'menu_class'      => 'menu', 
 		'echo'            => false,
-		'fallback_cb'     => $fallback_cb,
-		'before'          => $before,
-		'after'           => $after,
-		'link_before'     => $link_before,
-		'link_after'      => $link_after,
-		'depth'           => $depth,
-		'walker'          => $walker,
-		'theme_location'  => $theme_location
+		'fallback_cb'     => 'wp_page_menu',
+		'depth'           => 0,
     ) );
 
 }
