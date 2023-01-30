2568088fcad1d55deb8f6606d5598b6e18b722c9
James Pederson <james@jpederson.com>
2023-01-29 19:00:10 -0600

    Move ads on solutions main page.

--- page-front-solutions.php
+++ page-front-solutions.php
@@ -42,8 +42,6 @@ if ( is_member() ) {
 	the_partner_logos();
 	print '</div>';
 
-	the_page_ads();
-
 	the_testimonials();
 
 	if ( have_posts() ) :
@@ -70,6 +68,8 @@ if ( is_member() ) {
 	the_accordions();
 
 	the_page_events_row();
+
+	the_page_ads();
 
 	the_page_articles();
 
