3456ee718a3b9816570de752021863bd488550e9
James Pederson <james@jpederson.com>
2023-02-03 23:35:45 -0600

    Add static header and footer for Salesforce.

---
+++ footer-static.php
@@ -0,0 +1,63 @@
+	</section>
+	
+		<footer class="footer">
+		<div class="footer-inner">
+		
+			<p class="phone">
+				<a href="tel:8009959064">800.995.9064</a>
+			</p>
+
+			<div class="columns">
+				<div class="column address">
+					<p><strong>Arizona</strong><br />
+2020 North Central Avenue, Ste. 530<br />
+Phoenix, AZ 85004</p>
+<p><strong>Colorado</strong><br />
+650 S. Cherry Street SE, Ste. 920<br />
+Denver, CO 80246</p>
+<p><strong>Idaho</strong><br />
+2710 W Sunrise Rim Rd, Ste. 100<br />
+Boise, ID 83705</p>
+				</div>
+				<div class="column address">
+					<p><strong>Oregon</strong><br />
+13221 SW 68TH Pkwy, Ste. 400<br />
+Tigard, OR 97223</p>
+<p><strong>Washington</strong><br />
+18000 International Blvd, Ste. 1102<br />
+Seatac, WA 98188</p>
+					<div class="social">
+						<p><a href="https://www.youtube.com/channel/UCx4WZCnp-2losXaGN4rCHUg"><img decoding="async" loading="lazy" width="70" height="70" class="alignnone size-full wp-image-117190" src="/wp-content/uploads/2023/01/social-youtube.png" alt="Youtube" /></a> <img decoding="async" loading="lazy" width="70" height="70" class="alignnone size-full wp-image-117189" src="/wp-content/uploads/2023/01/social-twitter.png" alt="twitter" /> <a href="https://www.linkedin.com/company/gowestcua/about/"><img decoding="async" loading="lazy" width="70" height="70" class="alignnone size-full wp-image-117188" src="/wp-content/uploads/2023/01/social-linkedin.png" alt="Linkedin" /></a> <img decoding="async" loading="lazy" width="70" height="70" class="alignnone size-full wp-image-117186" src="/wp-content/uploads/2023/01/social-facebook.png" alt="Facebook" /></p>
+					</div>
+				</div>
+				<div class="column menu">
+					<h4>Links</h4>
+					<div class="menu-footer-links-container"><ul id="menu-footer-links" class="menu"><li id="menu-item-115091" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-115091"><a href="/about-gowest/">About Us</a></li>
+<li id="menu-item-115092" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-115092"><a href="/assumption-of-risk/">Acknowledgment of Risk</a></li>
+<li id="menu-item-115093" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-115093"><a href="/contact-us-2/">Contact Us</a></li>
+<li id="menu-item-115094" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-115094"><a href="/directory/">Credit Union Directory</a></li>
+<li id="menu-item-117177" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-117177"><a href="/resource-centers/digital-media-resource-center/">For Reporters</a></li>
+<li id="menu-item-115095" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-115095"><a href="https://www.gowestfoundation.org">GoWest Foundation</a></li>
+<li id="menu-item-115098" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-115098"><a href="/solutions">GoWest Solutions</a></li>
+<li id="menu-item-115096" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-115096"><a href="/our-commitments/">Our Commitments</a></li>
+<li id="menu-item-115097" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-115097"><a href="/about/privacy-policy-2/">Privacy Policy</a></li>
+<li id="menu-item-115099" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-115099"><a href="/submit-your-news/">Submit Your News</a></li>
+<li id="menu-item-117174" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-117174"><a href="/speak-at-a-future-event/">Speak At A Future Event</a></li>
+<li id="menu-item-117176" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-117176"><a href="/wp-content/uploads/2022/07/Safelisting-for-GoWest-Final.pdf">Safelist Us</a></li>
+</ul></div>				</div>
+								<div class="column subscribe">
+					<h4>Customize Your Experience</h4>
+<p>Sign up for On The Go News, alerts, updates, event &amp; registration information, and more.</p>
+<a href="/subscribe" class="btn green" target="_top">Subscribe</a>
+				</div>
+							</div>
+
+			<p class="small">Copyright &copy; 2023 GoWest Credit Union Association. All Rights Reserved.</p>
+
+		</div>
+	</footer>
+	
+</div><!-- #container -->
+
+	</body>
+</html>
