4c9c95a9c5e05fece4e181992703006ec4c91853
James Pederson <james@jpederson.com>
2023-02-03 23:43:08 -0600

    Fix index.php article styles.

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ the_showcase();
 			$return = '';
 			// Start the Loop.
 			while ( have_posts() ) : the_post(); 
-				$return .= '<div class="entry">';
+				$return .= '<div class="article-card entry">';
 				$return .= '<div class="entry-thumbnail">';
 				$return .= '<a href="' . get_the_permalink() . '" class="no-line">';
 				$return .= get_the_post_thumbnail( null, array( 768, 480 ) );
