42dacc4aada605354486fc4c49a5f669d952c3a5
James Pederson <james@jpederson.com>
2023-02-13 23:04:06 +0000

    Fix job manager template.

--- page-job-manager.php
+++ page-job-manager.php
@@ -22,21 +22,6 @@ if ( isset( $_GET['del'] ) ) {
 }
 
 
-// if the salesforce user is empty and the wp user is logged in
-if ( empty( $_SESSION['sf_user']['email'] ) && is_user_logged_in() ) {
-
-	// get the wp user
-	$the_user = wp_get_current_user();
-
-	// if there's an email address set
-	if ( isset( $the_user->data->user_email ) ) {
-
-		// set the salesforce email to the wp user email value
-		$_SESSION['sf_user']['email'] = $the_user->data->user_email;
-	}
-}
-
- 
 get_header();
 
 
@@ -69,52 +54,47 @@ $job_count = $the_query->found_posts;
 
 
 // output the title
-the_page_title();
-
 ?>
+<div class="page-title">
+	<h1><?php the_title(); ?></h1>
+</div>
 
-	<div class="two-column" role="main">
+	<div class="content-wide" role="main">
 
-		<div class="sidebar">
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('jobs-sidebar') ) : ?><!-- no sidebar --><?php endif; ?>
+		<div class="job-filter">
+			<div class="job-search"><label for="job-search">Search:</label> <input type="text" id="job-search" value="" placeholder="Search Jobs"></div>
+			<div class="job-count"><strong>Showing <?php print $job_count; ?> Job<?php print ( $job_count == 1 ? '' : 's' ) ?></strong></div>
 		</div>
-		
-		<div class="right-column">
-			<div class="job-filter">
-				<div class="job-search"><label for="job-search">Search:</label> <input type="text" id="job-search" value="" placeholder="Search Jobs"></div>
-				<div class="job-count"><strong>Showing <?php print $job_count; ?> Job<?php print ( $job_count == 1 ? '' : 's' ) ?></strong></div>
-			</div>
-			<div class="job-list">
-				<?php 
+		<div class="job-list">
+			<?php 
 
-				if ( $the_query->have_posts() ) : 
-					// Start the Loop.
-					while ( $the_query->have_posts() ) : $the_query->the_post(); 
-						global $post;
-						?>
-				<div class="entry-job group">
-					<div class="job-title">
-						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-					</div>
-					<div class="job-excerpt">
-						<?php if ( $_SESSION['sf_user']['email'] == get_cmb_value( 'job_creator' ) ) { ?><a href="<?php print $job_mgr_url; ?>?del=<?php the_ID(); ?>" class="job-delete" onClick="return confirm('Are you sure you want to delete that job?');">Delete Job</a><?php } ?>
-						<p class="job-info"><?php print ( has_cmb_value( 'job_company' ) ? get_cmb_value( 'job_company' ) : '' ); ?><?php print ( has_cmb_value( 'job_region' ) ? " | " .get_cmb_value( 'job_region' ) : '' ); ?></p>
-						<?php the_excerpt(); ?>
-					</div>
+			if ( $the_query->have_posts() ) : 
+				// Start the Loop.
+				while ( $the_query->have_posts() ) : $the_query->the_post(); 
+					global $post;
+					?>
+			<div class="entry-job group">
+				<div class="job-title">
+					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 				</div>
-						<?php
-					endwhile;
-				
-				else :
-				
-					// If no content, include the "No posts found" template.
-					get_template_part( 'content', 'none' );
-				
-				endif;
-
-				?>
-
+				<div class="job-excerpt">
+					<?php if ( $_SESSION['sf_user']['email'] == get_cmb_value( 'job_creator' ) ) { ?><a href="<?php print $job_mgr_url; ?>?del=<?php the_ID(); ?>" class="job-delete" onClick="return confirm('Are you sure you want to delete that job?');">Delete Job</a><?php } ?>
+					<p class="job-info"><?php print ( has_cmb_value( 'job_company' ) ? get_cmb_value( 'job_company' ) : '' ); ?><?php print ( has_cmb_value( 'job_region' ) ? " | " .get_cmb_value( 'job_region' ) : '' ); ?></p>
+					<?php the_excerpt(); ?>
+				</div>
 			</div>
+					<?php
+				endwhile;
+			
+			else :
+			
+				// If no content, include the "No posts found" template.
+				get_template_part( 'content', 'none' );
+			
+			endif;
+
+			?>
+
 		</div>
 
 	</div><!-- #content -->
