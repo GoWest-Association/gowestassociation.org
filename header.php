2a5bd4bd96a3f18ac9a6ece28a3a736683e481e9
James Pederson <james@jpederson.com>
2023-03-25 13:59:13 +0000

    Community widget styles.

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <![endif]-->
 
 <?php wp_head(); ?>
-<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=64" rel="stylesheet" type="text/css">
+<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=65" rel="stylesheet" type="text/css">
 
 
 <?php if ( is_foundation() ) { ?>
