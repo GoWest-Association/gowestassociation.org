45c2abd8356efa736cd52f08de1815d8629a4464
James Pederson <james@jpederson.com>
2025-07-28 14:32:02 +0000

    Add event tracking to Advocacy on the Move.

--- js/src/advocacy-on-the-move.js
+++ js/src/advocacy-on-the-move.js
@@ -5,13 +5,68 @@ jQuery(document).ready(function($){
 	if ( $( '.aotm-listing' ).length ) {
 		var do_filtering = function(){
 			var show_arizona = $( '.arizona-filter' ).is(':checked');
+			if ( show_arizona ) {
+				gtag('event', 'screen_view', {
+					'app_name': 'advocacyStateSelect',
+					'screen_name': 'arizona'
+				});
+			}
+
 			var show_colorado = $( '.colorado-filter' ).is(':checked');
+			if ( show_colorado ) {
+				gtag('event', 'screen_view', {
+					'app_name': 'advocacyStateSelect',
+					'screen_name': 'colorado'
+				});
+			}
+
 			var show_idaho = $( '.idaho-filter' ).is(':checked');
+			if ( show_idaho ) {
+				gtag('event', 'screen_view', {
+					'app_name': 'advocacyStateSelect',
+					'screen_name': 'idaho'
+				});
+			}
+
 			var show_oregon = $( '.oregon-filter' ).is(':checked');
+			if ( show_oregon ) {
+				gtag('event', 'screen_view', {
+					'app_name': 'advocacyStateSelect',
+					'screen_name': 'oregon'
+				});
+			}
+
 			var show_washington = $( '.washington-filter' ).is(':checked');
+			if ( show_washington ) {
+				gtag('event', 'screen_view', {
+					'app_name': 'advocacyStateSelect',
+					'screen_name': 'washington'
+				});
+			}
+
 			var show_wyoming = $( '.wyoming-filter' ).is(':checked');
+			if ( show_wyoming ) {
+				gtag('event', 'screen_view', {
+					'app_name': 'advocacyStateSelect',
+					'screen_name': 'wyoming'
+				});
+			}
+
 			var show_regulatory = $( '.regulatory-filter' ).is(':checked');
+			if ( show_regulatory ) {
+				gtag('event', 'screen_view', {
+					'app_name': 'advocacyStateSelect',
+					'screen_name': 'regulatory'
+				});
+			}
+
 			var show_federal = $( '.federal-filter' ).is(':checked');
+			if ( show_federal ) {
+				gtag('event', 'screen_view', {
+					'app_name': 'advocacyStateSelect',
+					'screen_name': 'federal'
+				});
+			}
 
 			var advocacy_blog = $( '.aotm-listing' );
 			advocacy_blog.find('.entry').each(function(){
