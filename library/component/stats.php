1d3def4408d9337e93340cbb21b82d22df26ba57
James Pederson <james@jpederson.com>
2025-10-02 14:13:31 +0000

    Add stats component.

---
+++ library/component/stats.php
@@ -0,0 +1,18 @@
+<?php
+
+if ( have_rows( 'point' ) ) :
+    ?>
+<div class="stats-container">
+    <?php 
+    while ( have_rows( 'point' ) ) : the_row();
+        ?>
+    <div class="stat <?php the_sub_field( 'color' ); ?>">
+        <div class="stat-number"><span><?php the_sub_field( 'data_point' ); ?></span></div>
+        <div class="stat-description"><?php the_sub_field( 'description' ); ?></div>
+    </div><?php
+    endwhile;
+    ?>
+</div>
+    <?php
+endif;
+
